5b54b78be42ba68d6a974740419df0561162c549
Josué Salazaku <josuesalazaku@gmail.com>
2023-11-17 10:47:45 +0100

    Import WMO for weather pictures

--- script.js
+++ script.js
@@ -1,4 +1,5 @@
 "use strict";
+import { WMO } from "./wmoWeather";
 
 const currentTemp = document.querySelector("#currentTemp");
 const country = document.querySelector("#country");
@@ -7,21 +8,21 @@ const fetchBtn = document.querySelector(".fetch-btn");
 const forecast = document.querySelector("#forecast");
 
 const daysOfTheWeek = [
-	"Sunday",
 	"Monday",
 	"Tuesday",
 	"Wednesday",
 	"Thursday",
 	"Friday",
 	"Saturday",
+	"Sunday",
 ];
 
 const weatherSvgs = {
-	clear: "/weather svgs/day.svg",
-	cloudy: "/weather svgs/cloudy.svg",
-	rain: "/weather svgs/rainy.svg",
-	snow: "/weather svgs/snowy-6.svg",
-	thunder: "/weather svgs/thunder.svg",
+	clear: "weather-svgs/day.svg",
+	cloudy: "weather-svgs/cloudy.svg",
+	rain: "weather-svgs/rainy.svg",
+	snow: "weather-svgs/snowy-6.svg",
+	thunder: "weather-svgs/thunder.svg",
 };
 
 async function getGeoData(location) {
@@ -33,7 +34,7 @@ async function getGeoData(location) {
 }
 
 async function getWeatherData(latitude, longitude) {
-	const weatherUrl = `https://api.open-meteo.com/v1/forecast?latitude=${latitude}&longitude=${longitude}&current=temperature_2m,relative_humidity_2m,apparent_temperature,is_day,rain,weather_code&hourly=temperature_2m,relative_humidity_2m,rain,weather_code&daily=weather_code,temperature_2m_max,temperature_2m_min&timezone=Europe%2FLondon&past_days=5`;
+	const weatherUrl = `https://api.open-meteo.com/v1/forecast?latitude=${latitude}&longitude=${longitude}&current=temperature_2m,is_day,rain,weather_code&hourly=temperature_2m,rain,weather_code&daily=weather_code,temperature_2m_max,temperature_2m_min&timezone=Europe%2FLondon`;
 	const response = await fetch(weatherUrl);
 	const data = await response.json();
 	return data;
@@ -50,32 +51,29 @@ async function appWeather() {
 
 		if (geo) {
 			const weatherData = await getWeatherData(geo.latitude, geo.longitude);
-
 			console.log("Weather data:", weatherData);
 
-			// Display country and current temperature
 			country.innerHTML = `${geo.country}`;
 			currentTemp.innerHTML = `Temperature: ${weatherData.hourly.temperature_2m[0]}°C <br> 
 		  	Max Temp: ${weatherData.daily.temperature_2m_max[0]}°C  <br> 
 		 	Min Temp: ${weatherData.daily.temperature_2m_min[0]}°C`;
-
-			forecast.innerHTML = ""; // Clear previous content
-
-			// ...
-
+			forecast.innerHTML = "";
 			for (let i = 0; i < 7; i++) {
 				const dayIndex = (new Date().getDay() + i) % 7;
 				const day = daysOfTheWeek[dayIndex];
 				const maxTemp = weatherData.daily.temperature_2m_max[i];
 				const minTemp = weatherData.daily.temperature_2m_min[i];
-
 				const forecastItem = document.createElement("div");
 
+				const weatherSvgElement = document.createElement("img");
+				weatherSvgElement.src = getWeatherSvg(
+					weatherData.daily.weather_code[i]
+				);
+				weatherSvgElement.alt = "Weather Icon";
+
 				const dayAndTempElement = document.createElement("p");
-				dayAndTempElement.textContent = `${day}: Max Temp: ${maxTemp}°C, Min Temp: ${minTemp}°C`;
-
+				dayAndTempElement.innerHTML = `<b>${day}<b> <br> Max Temp: ${maxTemp}°C<br> Min Temp: ${minTemp}°C`;
 				forecastItem.appendChild(dayAndTempElement);
-
 				forecast.appendChild(forecastItem);
 			}
 		} else {
@@ -84,6 +82,18 @@ async function appWeather() {
 	} catch (error) {
 		console.error("not weather:", error);
 	}
+}
+
+function getWeatherSvg(weatherCode) {
+	const weatherSvgs = {
+		clear: "/weather-svgs/day.svg",
+		cloudy: "/weather-svgs/cloudy.svg",
+		rain: "/weather-svgs/rainy.svg",
+		snow: "/weather-svgs/snowy-6.svg",
+		thunder: "/weather-svgs/thunder.svg",
+	};
+
+	return weatherSvgs[weatherCode] || "/weather-svgs/default.svg";
 }
 
 locationInput.addEventListener("keyup", (event) => {
